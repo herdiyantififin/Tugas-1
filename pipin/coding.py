1dc0d858ba074cc26682f02b90585f57ecdd770c
herdiyantififin <113223438+herdiyantififin@users.noreply.github.com>
2022-09-10 10:26:59 +0700

    Add files via upload
    menambahkan folder

---
+++ pipin/coding.py
@@ -0,0 +1,44 @@
+from turtle import st
+
+
+def fungsi_phyton():
+    print("hai ini adalah python")
+
+nama = "Herdiyanti Fifin Purwaningrum"
+umur = "18"
+ukuranSepatu = 38,5
+print("Nama saya adalah" + nama + "umur saya adalah" + str(umur))
+print("Umur saya adalah:",umur)
+print(ukuranSepatu,"dia adalah ",type(ukuranSepatu))
+
+
+
+#boolean
+masukkelas = True
+pulangkelas = False
+
+#List
+Hewan = ["Ayam","Kucing","kelinci","Anjing"]
+print(Hewan[1])
+
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print("Penjumlahan :"+ str(angka1 + angka2 + angka3))
+
+print("Pengurangan :"+ str(angka1 - angka2 - angka3))
+
+print("Pembagian 2 :"+ str(angka1 / angka2))
+
+print("Pembagian 2:"+str(75//10))
+
+print("Perkalian:" + str(angka1 * angka2))
+
+print("modulus:"+ str(75//10))
+
+
+
+fungsi_phyton()
+# INI KOMENTAR 
+# INI BUKAN KOMENTAR
+# ctrl + /
